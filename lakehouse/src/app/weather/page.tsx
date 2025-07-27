1e0c06ac9ecdcd5a4316f9ce5563a08f4c052dba
LandonSer <landonps@gmail.com>
2025-07-27 14:51:13 -0400

    created weather component

---
+++ lakehouse/src/app/weather/page.tsx
@@ -0,0 +1,16 @@
+import WeatherBar from "@/components/weather-bar";
+import { mockWeatherText } from "../../../data/mock-data";
+import { mockHeaderImg } from "../../../data/mock-data";
+
+export default function Weather() {
+    const weatherProps = {
+            weatherText: mockWeatherText,
+            weatherImage: mockHeaderImg[0].url,
+        };
+
+    return (
+        <section className="py-12 border-t border-border">
+            <WeatherBar weatherValues = {weatherProps} /> 
+        </section>
+    )
+}
